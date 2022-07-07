34b65c0af40610f4c53c91dabc28503947fb7272
pablixs <pablicjs@gmail.com>
2022-07-07 00:43:52 -0300

    db

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -1,9 +1,15 @@
 var mysql = require('mysql');
+// var con = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'bouvier'
+// });
 var con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'bouvier'
+    host: 'us-cdbr-east-06.cleardb.net',
+    user: 'b3db9304df35fe',
+    password: '927b421b',
+    database: 'heroku_f0d84c64f510881'
 });
 
 con.connect(
